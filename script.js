418ef9277ca2d4d3e023ccff8087c7c196a1e9a8
tyonweekes <92998721+tyonweekes@users.noreply.github.com>
2022-11-08 16:54:45 -0500

    Fixed width issue
    Fixed width issue

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ const map = L.map('map').setView([51.505, -0.09], 13);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
 // Create custom map pin
